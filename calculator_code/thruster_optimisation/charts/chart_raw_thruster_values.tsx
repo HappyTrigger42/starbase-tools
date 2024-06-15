223027adb007c5761768703f4a464ca2cf37b071
HappyTrigger42 <HappyTrigger42>
2024-06-15 22:13:15 +0000

    0.7.3

--- calculator_code/thruster_optimisation/charts/chart_raw_thruster_values.tsx
+++ calculator_code/thruster_optimisation/charts/chart_raw_thruster_values.tsx
@@ -15,20 +15,31 @@ export function get_chart_raw_thruster_values(chart_voxel_volume: Chart_data_int
         throw new Error("All charts must have the same length")
     }
     for (let i = 0; i < chart_voxel_volume.length; i++) {
-        const total_thrust = round(chart_voxel_volume[i].data[chart_voxel_volume[i].data.length - 1][1], chart_radio_rounding_factor)
-        const total_mass_tons = round(chart_mass[i].data[chart_voxel_volume[i].data.length - 1][0], chart_radio_rounding_factor)
-        const total_elec = round(chart_elec_usage[i].data[chart_voxel_volume[i].data.length - 1][0], chart_radio_rounding_factor)
-        const total_prop = round(chart_prop_usage[i].data[chart_voxel_volume[i].data.length - 1][0], chart_radio_rounding_factor)
-        const total_kilo_voxel_volume = round(chart_voxel_volume[i].data[chart_voxel_volume[i].data.length - 1][0], chart_radio_rounding_factor)
-        const buffer: chart_raw_thruster_values = {
-            name: chart_voxel_volume[i].name,
-            total_thrust: total_thrust,
-            total_mass_tons: total_mass_tons / 1000,
-            total_elec: total_elec,
-            total_prop: total_prop,
-            total_kilo_voxel_volume: total_kilo_voxel_volume / 1000,
+        if (chart_voxel_volume[i].data.length > 0) {
+            const total_thrust = round(chart_voxel_volume[i].data[chart_voxel_volume[i].data.length - 1][1], chart_radio_rounding_factor)
+            const total_mass_tons = round(chart_mass[i].data[chart_voxel_volume[i].data.length - 1][0], chart_radio_rounding_factor)
+            const total_elec = round(chart_elec_usage[i].data[chart_voxel_volume[i].data.length - 1][0], chart_radio_rounding_factor)
+            const total_prop = round(chart_prop_usage[i].data[chart_voxel_volume[i].data.length - 1][0], chart_radio_rounding_factor)
+            const total_kilo_voxel_volume = round(chart_voxel_volume[i].data[chart_voxel_volume[i].data.length - 1][0], chart_radio_rounding_factor)
+            const buffer: chart_raw_thruster_values = {
+                name: chart_voxel_volume[i].name,
+                total_thrust: total_thrust,
+                total_mass_tons: total_mass_tons / 1000,
+                total_elec: total_elec,
+                total_prop: total_prop,
+                total_kilo_voxel_volume: total_kilo_voxel_volume / 1000,
+            }
+            ret.push(buffer)
+        } else {
+            ret.push({
+                name: chart_voxel_volume[i].name,
+                total_thrust: 0,
+                total_mass_tons: 0,
+                total_elec: 0,
+                total_prop: 0,
+                total_kilo_voxel_volume: 0,
+            })
         }
-        ret.push(buffer)
     }
     return ret
 }
